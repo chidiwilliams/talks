af0bf1a3a852452bf9cd3394f1f6e4945413f3d0
Chidi Williams <williamschidi1@gmail.com>
2023-09-16 14:46:20 +0100

    Updates

--- 2023-hearing-between-the-lines/components/Spectrum.vue
+++ 2023-hearing-between-the-lines/components/Spectrum.vue
@@ -138,6 +138,6 @@ function stopRecording() {
       >
       <Button v-on:click="stopRecording()" v-if="isRecordingRef">Stop</Button>
     </div>
-    <canvas id="chart" style="width: 100%; max-width: 100%"></canvas>
+    <canvas id="chart" style="width: 90%; max-width: 90%"></canvas>
   </div>
 </template>
